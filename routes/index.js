5fd966b06b4818d235ba8f1b3c67f1e06a4cad3b
whothat <benhuff322@gmail.com>
2016-02-21 20:55:10 -0800

    create about page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'qGen' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'qGen | About' });
+});
+
+
 module.exports = router;
