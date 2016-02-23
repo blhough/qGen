dfd9dadb38f82fecc670eb388c7fd1a70aa54493
whothat <benhuff322@gmail.com>
2016-02-22 23:01:38 -0800

    working on generator

--- routes/gen.js
+++ routes/gen.js
@@ -2,22 +2,24 @@ var express = require('express');
 var router = express.Router();
 //var gen = require('/helpers/generator');
 
+var title = "Generate"
+
 /* GET home page. */
 router.get('/:category/:type', function(req, res, next) {
-  res.render('generator', { questions: ["how fast is the ball going?","what color is the light?"] });
+  res.render('generator', { title: title });
   console.log(req.params.category + ' ' + req.params.type);
 });
 
 /* GET home page. */
-router.get('/:category', function(req, res, next) {
-  res.render('index', { title: 'test test' });
+router.get('/:chapter', function(req, res, next) {
+  res.render('generator', { title: title });
   console.log(req.params.category );
 });
 
 
 /* GET home page. */
-router.get('/gen/test', function(req, res, next) {
-  res.render('index', { title: 'test gen' });
+router.get('/', function(req, res, next) {
+  res.render('generator', { title: title });
 });
 
 module.exports = router;
